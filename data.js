dac0ec74e1dd4f8a208d7be235158356fec453b2
Ebtisam <ebtisam21@gmail.com>
2023-07-09 23:55:09 -0700

    hw2 created

---
+++ data.js
@@ -0,0 +1,9 @@
+let cars = [
+    { model : "toyota", make : "camry", color : "black", year: 2020, price: 24000 },
+    { model : "honda", make : "acord", color : "brown", year: 2021, price: 26000 },
+    { model : "altima", make : "nissan", color : "white", year: 2018, price: 17000 },
+    { model : "mx-5", make : "mazda", color : "blue", year: 2022, price: 29000 },
+    { model : "x5", make : "bmw", color : "red", year: 2015, price: 12000 }
+    
+];
+console.log(cars);
