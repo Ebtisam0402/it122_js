fbff60a4bbec9af8d5e16fe60638b0e84ca76ab2
Ebtisam <ebtisam21@gmail.com>
2023-07-12 03:27:11 -0700

    updated

--- data.js
+++ data.js
@@ -1,9 +1,28 @@
+import { model } from "mongoose";
+
 let cars = [
-    { model : "toyota", make : "camry", color : "black", year: 2020, price: 24000 },
-    { model : "honda", make : "acord", color : "brown", year: 2021, price: 26000 },
-    { model : "altima", make : "nissan", color : "white", year: 2018, price: 17000 },
-    { model : "mx-5", make : "mazda", color : "blue", year: 2022, price: 29000 },
-    { model : "x5", make : "bmw", color : "red", year: 2015, price: 12000 }
+    { model : "Toyota", make : "camry", color : "black", year: 2020, price: 24000 },
+    { model : "Honda", make : "acord", color : "brown", year: 2021, price: 26000 },
+    { model : "Altima", make : "nissan", color : "white", year: 2018, price: 17000 },
+    { model : "MX-5", make : "mazda", color : "blue", year: 2022, price: 29000 },
+    { model : "X5", make : "bmw", color : "red", year: 2015, price: 12000 }
     
 ];
-console.log(cars);
+
+
+
+
+function getAll() {
+  return cars;
+}
+
+
+function  getItem () {
+  return cars.find(cars => cars.model === model)  
+}
+  
+
+
+
+
+export { getAll, getItem };
