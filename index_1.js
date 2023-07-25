cbe07cbf94930d1baee6df235a879f4ba311a14f
Ebtisam <ebtisam21@gmail.com>
2023-07-24 22:05:56 -0700

    discard soon

--- index_1.js
+++ index_1.js
@@ -6,13 +6,15 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('./public'));// set location for static files
 app.set('view engine', 'ejs');
+app.use(express.json()); //Used to parse JSON bodies
+
+import cors from 'cors';
+app.use('/api', cors()); // set Access-Control-Allow-Origin header for api route
 
 app.get('/about', (req,res) => {
   console.log(req.url)
   res.send('This is the about page')
 })
-
-
 
 app.get('/', (req, res, next) => {
   Car.find({}).lean()
@@ -31,6 +33,18 @@ app.get('/detail', (req,res,next) => {
       })
       .catch(err => next(err));
 });
+
+app.post('/detail', (req,res, next) => {
+  // insert or update a single record
+const newBook = {'title':'dune', 'author':'frank herbert', 'pubdate': 1963 }
+Book.updateOne({'title':'dune'}, newBook, {upsert:true}, (err, result) => {
+  if (err) return next(err);
+  console.log(result);
+  // other code here
+})
+      .catch(err => next(err));
+});
+
 
 
 // define 404 handler
